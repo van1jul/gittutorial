b33c05d47bb552150dd478088187a4aaa4e96ab0
van1jul <w5m8@ugrad.cs.ubc.ca>
2015-10-02 02:05:34 -0700

    heloo

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -67,8 +67,7 @@ public class GitLab implements EntryPoint {
 
 			@Override
 			public void onSuccess(List<IUser> result) {
-				Window.alert("Got list back with " +  result.size() + " entries");
-				
+			displayUsers(result);
 			}});
 	
 //<<<<<<< HEAD
